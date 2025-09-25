e70de8c0aac54b1e690482e7a9ec5addf713a41c
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-24 19:02:44 -0500

    generics_struct_definitions4

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
-struct Point<T, U> {
-    x: T,
-    y: U,
-}
 
-fn main() {
+
+fn generic_structs_definitions() {
+
+    struct Point<T, U> {
+        x: T,
+        y: U,
+    }
+
     let both_integer = Point { x: 5, y: 10 };
     println!("both_integer.x, both_integer.y: {}, {}", both_integer.x, both_integer.y);
-    
+
     let both_float = Point { x: 1.0, y: 4.0 };
     println!("both_float.x, both_float.y: {}, {}", both_float.x, both_float.y);
 
     let integer_and_float = Point { x: 5, y: 4.0 };
     println!("integer_and_float.x, integer_and_float.y: {}, {}", integer_and_float.x, integer_and_float.y);
+}
+
+fn main() {
+    generic_structs_definitions();
 }
