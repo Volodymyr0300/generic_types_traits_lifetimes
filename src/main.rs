016341ded13b5ed67d3ab30b2e676c6c421c87d7
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-26 13:28:00 -0500

    generic_in_method_definitions2

--- src/main.rs
+++ src/main.rs
@@ -72,29 +72,16 @@ fn generic_in_method_definitions() {
         let p2 = Point { x: "Hello", y: 'c' }; // V is &str, W is char
         let p3 = p1.mixup(p2); // T is i32, W is char from p2
         println!("p3.x, p3.y: {}, {}", p3.x, p3.y);
+        
     }
-}
-struct Point<T, U> {
-            x: T,
-            y: U,
-        }
 
-impl<T, U> Point<T, U> {
-            fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-                Point {
-                    x: self.x,
-                    y: other.y,
-                }
-            }
-        }
+    generic_in_method_definitions_T();
+    generic_in_method_definitions_T_U();
+}
+
 
 fn main() {
     generic_structs_definitions();
     generic_enum_definitions();
     generic_in_method_definitions();
-
-    let p1 = Point { x: 5, y: 10.4 }; // T is i32, U is f64
-        let p2 = Point { x: "Hello", y: 'c' }; // V is &str, W is char
-        let p3 = p1.mixup(p2); // T is i32, W is char from p2
-        println!("p3.x, p3.y: {}, {}", p3.x, p3.y);
 }
